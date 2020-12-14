d0a2dd61e4916c621d757ff60a03cc9dd19123b0
Claire Smith <clairesmith261104@gmail.com>
2020-12-15 08:16:22 +1100

    Update CommandLine_TicTacToe.cpp

--- CommandLine_TicTacToe.cpp
+++ CommandLine_TicTacToe.cpp
@@ -3,7 +3,7 @@
 #include <array>
 
 void displayBoard(const std::array<uint8_t, 9>& board) {
-	char t[] { '-', 'X', 'O' }; // Converts board data to displayable text
+	char t[] { '-', 'X', 'O' };
 	std::cout << std::endl <<
 		t[board[0]] << " " << t[board[1]] << " " << t[board[2]] << std::endl <<
 		t[board[3]] << " " << t[board[4]] << " " << t[board[5]] << std::endl <<
@@ -19,16 +19,17 @@ int main(void) {
 		"7 8 9" << std::endl;
 
 	// 0 = empty, 1 = X, 2 = O
-	std::array<uint8_t, 9> board{};
+	std::array<uint8_t, 9> board {};
 
 	// false = X, true = O
+	// uint8_t(player) + 1 converts bool (X=0, O=1) to same format as board array (X=1, O=2)
 	bool player = false;
 
-	// Main game loop
+	// Main loop
 	while (true) {
 		displayBoard(board);
 
-		// Get input
+		// Retrieve input
 		bool retryInput;
 		do {
 			retryInput = false;
@@ -40,16 +41,15 @@ int main(void) {
 
 			// Validate input
 			input--;
-			if (input >= 9) { // Reject input if out of bound
+			if (input < 0 || input >= 9) {
 				std::cout << "Please enter a number between 1 and 9!\a" << std::endl;
 				retryInput = true;
 			}
-			else if (board[input]) { // Reject input if chosen space unavailable
+			else if (board[input]) {
 				std::cout << "That space is taken! Choose another space!\a" << std::endl;
 				retryInput = true;
 			}
-			else board[input] = player + 1; // Accept input
-
+			else board[input] = uint8_t(player) + 1;
 		} while (retryInput);
 
 
@@ -57,25 +57,24 @@ int main(void) {
 		bool win = false;
 		for (int i = 0; i < 3; i++) {
 			win |= ( // Check row i
-				player + 1 == board[i * 3 + 0] &&
-				player + 1 == board[i * 3 + 1] &&
-				player + 1 == board[i * 3 + 2]
+				uint8_t(player) + 1 == board[i * 3 + 0] &&
+				uint8_t(player) + 1 == board[i * 3 + 1] &&
+				uint8_t(player) + 1 == board[i * 3 + 2]
 				) || ( // Check column i
-				player + 1 == board[0 + i] &&
-				player + 1 == board[3 + i] &&
-				player + 1 == board[6 + i]
+				uint8_t(player) + 1 == board[0 + i] &&
+				uint8_t(player) + 1 == board[3 + i] &&
+				uint8_t(player) + 1 == board[6 + i]
 			);
 		}
-
 		// Check for both diagonals
 		win |= (
-			player + 1 == board[0] &&
-			player + 1 == board[4] &&
-			player + 1 == board[8]
+			uint8_t(player) + 1 == board[0] &&
+			uint8_t(player) + 1 == board[4] &&
+			uint8_t(player) + 1 == board[8]
 			) || (
-			player + 1 == board[2] &&
-			player + 1 == board[4] &&
-			player + 1 == board[6]
+			uint8_t(player) + 1 == board[2] &&
+			uint8_t(player) + 1 == board[4] &&
+			uint8_t(player) + 1 == board[6]
 		);
 
 		// Display winning text
@@ -100,5 +99,6 @@ int main(void) {
 		player = !player;
 	}
 
+	
 	return 0;
 }
